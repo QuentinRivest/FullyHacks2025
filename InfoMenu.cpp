c32dee2f83b0fb130df069608ce2b86d48870f6d
eccortes4 <eccortes4@gmail.com>
2025-04-13 12:34:53 -0700

    Finished start UI

--- InfoMenu.cpp
+++ InfoMenu.cpp
@@ -5,27 +5,38 @@
 #include <chrono>
 #include <thread>
 
-void InfoMenu(sf::RenderWindow& window) {
+void InfoMenu() {
     using namespace std::chrono;
-    
-    sf::Font code_font;
-    if (!code_font.loadFromFile("assets/code_font.ttf"))
-    {
-    std::cout << "Error";
+
+    unsigned int win_width  = 800;
+    unsigned int win_length = 600;
+    int count = 0;
+    sf::RenderWindow window(sf::VideoMode({win_width, win_length}), "SFML works!");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+          if (event.type == sf::Event::Closed) {
+            window.close();
+          }
+        }
+        if (count < 1) {
+        window.clear(sf::Color::Black);
+        sf::Font code_font;
+        if (!code_font.loadFromFile("assets/code_font.ttf")) {
+            std::cout << "Error";
+        }
+        std::string message = "During an inter galactic mission a fleet of aliens has fully hacked your power systems.\nThe oxygen generation systems can only function for another 15 minutes.\nThe aliens have also hacked the door systems on board.\nYou must solve the now puzzling door systems to reach the escape pod or face certain death.\n";
+        std::string mess;
+        for (int i = 0; i < message.length(); ++i) {
+            mess += message[i];
+            sf::Text text(mess, code_font, 15);
+            window.draw(text);
+            window.display();
+            std::this_thread::sleep_for(milliseconds(80));
+        }
+        count += 1;
+            }    
     }
-    std::string message = "During an inter galactic mission a fleet of aliens has fully hacked your power systems.\nThe oxygen generation systems can only function for another 15 minutes.\nThe aliens have also hacked the door systems on board.\nYou must solve the now puzzling door systems to reach the escape pod or face certain death.\n";
-    std::string mess;
-    for (int i = 0; i < message.length(); ++i) {
-        mess += message[i];
-        sf::Text text(mess, code_font, 15);
-        window.draw(text);
-        window.display();
-        std::this_thread::sleep_for(milliseconds(90));
-    }
-    
-    sf::Text text(message, code_font, 50);
-    window.draw(text);
-    
     
 }
 
--- TitleScreen.cpp
+++ TitleScreen.cpp
@@ -2,50 +2,45 @@
 #include <cmath>
 #include "Button.cpp"
 
-void TitleScreen(sf::RenderWindow& window){
-    sf::Font space_font;
-    space_font.loadFromFile("assets/space-font-italic.ttf");
-    Button start(600, 520, "assets/Solid_black.png");
-    sf::Text Start("START", space_font, 40);
-    Start.move(550, 500);
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-          if (event.type == sf::Event::Closed) {
-            window.close();
-          }
-        }
-        start.update(window);
-        window.clear(sf::Color::Black);
-
-        sf::Text title("LAST BREATH", space_font, 50);
-        auto center = title.getGlobalBounds().width / 2.f;
-        auto localBounds = center + title.getLocalBounds().width / 2;
-        auto rounded = round(localBounds);
-        title.setOrigin(rounded, title.getLocalBounds().height);
-        title.setPosition(sf::Vector2f{ window.getSize() / 2u });
-        start.render(window);
-        window.draw(Start);
-        window.draw(title);
-        window.display();
-    if (start.isPressed()) {
-        std::cout << "Click";
-        window.clear(sf::Color::Black);
-        InfoMenu(window);
-    }
-    }
-
-    
-    
-
-}
-
-int main() {
+void TitleScreen(){
     unsigned int win_width  = 800;
     unsigned int win_length = 600;
     sf::Font space_font;
     space_font.loadFromFile("assets/space-font-italic.ttf");
     sf::RenderWindow window(sf::VideoMode({win_width, win_length}), "SFML works!");
-    TitleScreen(window);
     
+    space_font.loadFromFile("assets/space-font-italic.ttf");
+    Button start(win_width * 0.58, win_length * 0.74, "assets/Solid_black.png");
+    start.setSize(.17 * win_width, .06 * win_length);
+    sf::Text Start("START", space_font, 40);
+    Start.move(win_width * 0.5, win_length *0.7);
+    sf::Text title("LAST BREATH", space_font, 50);
+    title.setPosition(win_width * 0.1, win_length * 0.5);
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+          if (event.type == sf::Event::Closed) {
+            window.close();
+            return;
+          }
+        }
+        start.update(window);
+        window.clear(sf::Color::Black);
+        
+        start.render(window);
+        window.draw(Start);
+        window.draw(title);
+        window.display();
+    if (start.isPressed()) {
+        Start.setFillColor(sf::Color::Red);
+        break;
+    }
+    }
+    window.clear(sf::Color::Black);
+    InfoMenu();
+
+}
+
+int main() {
+    TitleScreen();
 }
